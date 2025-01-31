25e5727dd8e907905cd2232617712dd907ea9057
avery.white <averydouglascwhite@usf.edu>
2025-01-31 11:46:16 -0500

    Task 1 - Edited comments

--- cc_4.js
+++ cc_4.js
@@ -1,2 +1,12 @@
 console.log(`cc_4.js has been successfully loaded!`);
 
+// Task 1 - If statement
+let purchaseAmount = 150; // Initial purchase amount
+let finalAmount = purchaseAmount; // Place holder for Initial Amount - Dicount
+console.log(`The original purchase: $${purchaseAmount}`); // Displays output
+if (purchaseAmount > 100) {
+    let discount = .15;
+    finalAmount = purchaseAmount - (purchaseAmount * discount); // Calculates discount if purchase amount is above $100
+}
+console.log(`The final amount after discount: $${finalAmount}`); // Displays output
+// Task 1 - End
